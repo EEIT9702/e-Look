278866620ffa031f9955ca88d79ab1e3a15fae50
彭至淳 <Jackson@EEIT>
2017-09-23 20:49:49 +0800

    第二次測試
    多加"拉拉拉"

--- e-Look/src/main/java/www/iiiedu/Jackson/e_Look/HelloServlet.java
+++ e-Look/src/main/java/www/iiiedu/Jackson/e_Look/HelloServlet.java
@@ -7,10 +7,10 @@ import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 
-@WebServlet(value="/hello", name="helloServlet")
+@WebServlet("/helloServlet")
 public class HelloServlet extends GenericServlet {
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-        res.getWriter().println("Hello world!!");
+        res.getWriter().println("Hello world!!拉拉拉");
     }
 }
