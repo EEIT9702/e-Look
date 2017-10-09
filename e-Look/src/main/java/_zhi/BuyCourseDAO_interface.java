a09394110c4212ecdca31ba0eb62f8b924052172
江致輝 <ooo@xxx>
2017-10-09 08:40:20 +0800

    依據命名格式修改JNDI
    詳細內容參照web.xml

--- e-Look/src/main/java/_zhi/BuyCourseDAO_interface.java
+++ e-Look/src/main/java/_zhi/BuyCourseDAO_interface.java
@@ -6,6 +6,6 @@ public interface BuyCourseDAO_interface {
 	 public void insert(BuyCourseVO memberVO);
      public void update(BuyCourseVO memberVO);
      public void delete(Integer memberID);
-     public BuyCourseVO  findByPrimaryKey(Integer memberID);
+     public BuyCourseVO findByPrimaryKey(Integer memberID);
      public List<BuyCourseVO> getAll();
 }
