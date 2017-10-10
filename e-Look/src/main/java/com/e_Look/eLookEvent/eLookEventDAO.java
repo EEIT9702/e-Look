50c07cf2a769b9f629eb70a263f91f4584c41ee6
李亦然 <luke710130@gmail.com>
2017-10-10 13:31:18 +0800

    修改錯誤
    修改BuyCourseDAO、courseClassDAO、eLookEventDAO，JNDI錯誤部分

--- e-Look/src/main/java/com/e_Look/eLookEvent/eLookEventDAO.java
+++ e-Look/src/main/java/com/e_Look/eLookEvent/eLookEventDAO.java
@@ -18,7 +18,7 @@ public class eLookEventDAO implements eLookEvent_interface{
 	static {
 		try {
 			Context ctx = new InitialContext();
-			ds = (DataSource) ctx.lookup("java:comp/env/jdbc/MemberDataBase");
+			ds = (DataSource) ctx.lookup("java:comp/env/jdbc/eLookDB");
 		} catch (NamingException e) {
 			e.printStackTrace();
 		}
