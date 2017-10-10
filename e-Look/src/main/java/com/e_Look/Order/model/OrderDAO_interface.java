48a95370da342ec8effdb1456c689067ea390dd4
黃世傑 <oo@xx>
2017-10-10 20:35:26 +0800

    Order訂單 vo,dao建立
    Order vo,dao建立

---
+++ e-Look/src/main/java/com/e_Look/Order/model/OrderDAO_interface.java
@@ -0,0 +1,9 @@
+package com.e_Look.Order.model;
+import java.util.List;
+public interface OrderDAO_interface {
+public void insert(OrderVO OrderVO);
+public void update(OrderVO OrderVO);
+public void delete(Integer orderID);
+public OrderVO findByPrimaryKey(Integer orderID);
+public List<OrderVO> getAll();
+}
