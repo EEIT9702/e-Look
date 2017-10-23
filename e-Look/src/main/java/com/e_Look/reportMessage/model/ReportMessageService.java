8456c47982d6b7ba8ea788d82a0decc1bc761544
徐子超 <eeit9729@iii.com>
2017-10-23 20:44:46 +0800

    修改ReportMessage
    修改ReportMessage

--- e-Look/src/main/java/com/e_Look/reportMessage/model/ReportMessageService.java
+++ e-Look/src/main/java/com/e_Look/reportMessage/model/ReportMessageService.java
@@ -1,5 +1,7 @@
 package com.e_Look.reportMessage.model;
 
+import java.util.List;
+
 import com.e_Look.message.model.MessageVO;
 
 public class ReportMessageService {
@@ -7,7 +9,7 @@ public class ReportMessageService {
 	public ReportMessageService() {
 		dao = new ReportMessageDAO();
 	}
-	public ReportMessageVO insertReportMessage(Integer messageID, Integer reportMemberID,
+	public void insertReportMessage(Integer messageID, Integer reportMemberID,
 			String reportContent) {
 		
 		ReportMessageVO reportMessageVO = new ReportMessageVO();
@@ -19,25 +21,27 @@ public class ReportMessageService {
 		reportMessageVO.setReportContent(reportContent);
 		dao.insert(reportMessageVO);
 		
-		return reportMessageVO;
+	}
+	
+	public List<ReportMessageVO>findNotHandle(Integer status) {
+		return dao.getNotHandle(status);
 	}
 
-	public ReportMessageVO updateReportMessageStatus(Integer reportId, Byte status) {
+	public void updateReportMessageStatus(Integer reportId, Byte status) {
 		
 		ReportMessageVO reportMessageVO = new ReportMessageVO();
 		reportMessageVO.setReportId(reportId);
 		reportMessageVO.setStatus(status);
 		dao.update(reportMessageVO);
 		
-		return reportMessageVO;
 	}
+	
 	//寫著,但應該用不到
-	public ReportMessageVO deleteReportMessage(Integer reportId) {
+	public void deleteReportMessage(Integer reportId) {
 		
 		ReportMessageVO reportMessageVO = new ReportMessageVO();
 		reportMessageVO.setReportId(reportId);
 		dao.update(reportMessageVO);
 		
-		return reportMessageVO;
 	}
 }
