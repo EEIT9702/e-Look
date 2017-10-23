5d1af907ce8f234f4942110b562ac85cec47ae3c
彭至淳 <jackson04140923@gmail.com>
2017-10-23 09:43:22 +0800

    利用Filter自動建立資料庫
    新增課程取得流水號

--- e-Look/src/main/java/com/e_Look/Course/CourseService.java
+++ e-Look/src/main/java/com/e_Look/Course/CourseService.java
@@ -7,20 +7,16 @@ public class CourseService {
 	public CourseService(){
 		dao=new CourseDAO();
 	}
-	public Integer insertCourseData(){
-		Connection conn = null;
-		
-		
-		CourseVO courseVO1 = new CourseVO();
-		courseVO1.setSoldPrice(0);
-		courseVO1.setCourseLength(0);
-		courseVO1.setTargetStudentNumber(0);
-		courseVO1.setStatus(0);
-		courseVO1.setMemberID(100001);
-		courseVO1.setAvgScore(0.0);
-		dao.insert(courseVO1);
-		
-		return null;
+	public Integer CreateNewCourse(){
+		CourseVO CourseVO1 =new CourseVO();
+		CourseVO1.setSoldPrice(0);
+		CourseVO1.setCourseLength(0);
+		CourseVO1.setTargetStudentNumber(0);
+		CourseVO1.setStatus(0);
+		CourseVO1.setMemberID(100001);
+		CourseVO1.setAvgScore(0.0);
+		Integer CourseID= dao.insert(CourseVO1);
+		return CourseID;
 	}
 	
 }
