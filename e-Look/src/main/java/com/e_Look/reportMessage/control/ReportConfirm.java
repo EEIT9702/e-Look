fae916e57da338aa77ba3094d8ae5016b84eae34
徐子超 <eeit9729@iii.com>
2017-10-24 11:57:51 +0800

    修改ReportConfirm & ReportMessage
    修改ReportConfirm & ReportMessage

--- e-Look/src/main/java/com/e_Look/reportMessage/control/ReportConfirm.java
+++ e-Look/src/main/java/com/e_Look/reportMessage/control/ReportConfirm.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ProductsInsert
  */
-@WebServlet("/ReportConfirm")
+@WebServlet("/backstage/ReportConfirm")
 public class ReportConfirm extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
