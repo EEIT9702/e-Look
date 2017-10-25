6c6fe738b2938ef487b508d61dc5822c75256552
徐子超 <eeit9729@iii.com>
2017-10-25 14:04:31 +0800

    修改CourseClassDetails
    修改CourseClassDetails

--- e-Look/src/main/java/com/e_Look/CourseClassDetails/CourseClassDetails_interface.java
+++ e-Look/src/main/java/com/e_Look/CourseClassDetails/CourseClassDetails_interface.java
@@ -7,7 +7,7 @@ import com.e_Look.courseClass.CourseClassVO;
 
 public interface CourseClassDetails_interface {
 	
-	public void insert(CourseClassVO courseClassVO, CourseVO courseVO);
+	public void insert(CourseVO courseVO, CourseClassVO courseClassVO);
 	public void delete(Integer courseID);
 	public void update(CourseClassVO courseClassVO, CourseVO courseVO);
 	public List<CourseClassDetailsVO> findBycourseClassID(Integer CourseClassID);
