daadaabe7147a55075234b514ceaf976fe780653
江致輝 <ooo@xxx>
2017-11-05 01:07:52 +0800

    這週末寫這支就好XD
    這週末寫這支就好XD

---
+++ e-Look/src/main/java/com/e_Look/eLookEvent/control/UpdateCourseClassEvent.java
@@ -0,0 +1,60 @@
+package com.e_Look.eLookEvent.control;
+
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.scheduling.config.Task;
+
+import com.e_Look.courseClass.CourseClassDAO;
+import com.e_Look.courseClass.CourseClassVO;
+import com.e_Look.eLookEvent.eLookEventDAO;
+import com.e_Look.eLookEvent.eLookEventVO;
+
+@WebServlet(urlPatterns = "/UpdateCourseClassEvent", loadOnStartup = 2)
+public class UpdateCourseClassEvent extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void init() throws ServletException {
+		// 時間設定在當天的零點零分零秒
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.HOUR_OF_DAY, 0);
+		calendar.set(Calendar.MINUTE, 0);
+		calendar.set(Calendar.SECOND, 0);
+		eLookEventDAO eDao = new eLookEventDAO();
+		CourseClassDAO ccDAO = new CourseClassDAO();
+		Date date = calendar.getTime();
+		Timer timer = new Timer();
+		timer.scheduleAtFixedRate(new TimerTask() {
+			@Override
+			public void run() {
+				List<eLookEventVO> elookeventVOs = eDao.getAll();
+				for (eLookEventVO elookeventVO : elookeventVOs){
+					if(elookeventVO.geteStartDate().getTime()<=System.currentTimeMillis() && elookeventVO.geteEndDate().getTime()+1000*60*60*24 >= System.currentTimeMillis()){
+						List<CourseClassVO> ccVOs = ccDAO.getAll();
+						for(CourseClassVO ccVO:ccVOs){
+							if(ccVO.equals(elookeventVO.getCourseClass1()) || ccVO.equals(elookeventVO.getCourseClass2()) || ccVO.equals(elookeventVO.getCourseClass3()) ){
+								ccVO.setEventVO(elookeventVO);
+								ccDAO.updateEventID(ccVO);
+							}
+						}
+						
+					}
+				}
+			}
+			// 於當日0點0分0秒開始計算 每一天做一次
+		}, date, 1000 * 60 * 60 * 24);
+
+	}
+
+}
